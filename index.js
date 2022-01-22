24b90883a6a19fb8ea98d43bdbb31bc288c3698a
RunSpaceSoftware <shayanfr1387@gmail.com>
2022-01-22 19:41:39 +0330

    some js changes

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hello World");
+const http = require('http');
+const dt = require('./modules')
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write("time" + dt.myDateTime());
+  res.end();
+}).listen(8080);
